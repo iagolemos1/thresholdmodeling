74a0b4fd733e66f1b721a9a87d84e3855eb79462
Iago Pereira Lemos <lemosiago123@gmail.com>
2020-01-25 20:20:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/iagolemos1/thresholdmodeling",
-    packages=['thresholdmodeling', 'tests'],
+    packages=['thresholdmodeling'],
     test_suit = 'tests',
     classifiers=[
         "Programming Language :: Python :: 3",
