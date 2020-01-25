aafe4c513251fd75f90af82badb6a17aaa11758e
Iago Pereira Lemos <lemosiago123@gmail.com>
2020-01-25 16:47:17 -0300

    Update __init__.py

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,1 +1,8 @@
 
+from rpy2.robjects.packages import importr
+import rpy2.robjects.packages as rpackages
+
+base = importr('base')
+utils = importr('utils')
+utils.chooseCRANmirror(ind=1)
+utils.install_packages('POT') #installing POT package
